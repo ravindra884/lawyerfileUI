f33efc68b7935b288b89abc520d4af24bce26d8d
ravindra <ravindra884@gmail.com>
2016-09-21 16:14:45 +0530

    added checkbox list in checklist section and added toggle slide effect on recent activities , new admin page html

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,18 @@ $(document).ready(function(){
 		$('.view.infolist').slideToggle();
 		$('.edit.infolist').slideToggle();
 	});
+	$('.changeviewBar .gridview').click(function() {
+		$('.checkboxlist').addClass('checkboxgrid');
+	});
+	$('.changeviewBar .listview').click(function() {
+		$('.checkboxlist').removeClass('checkboxgrid');
+	});
+
+	$('.screeningTitleBar .alignment').click(function(){
+		$('.rightpanel').toggleClass('faderight');
+		$('.middPanel').toggleClass('fullwidth');
+	});
+	$('.deals dl dt').click(function() {
+		$(this).next('dd').next('.addStages').slideToggle();
+	});
 });
